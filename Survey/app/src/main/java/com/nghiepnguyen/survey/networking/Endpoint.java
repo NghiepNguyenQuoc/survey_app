3fd31e859ee8a52e47d5f6562903120c971254ff
Nghiep Nguyen <nghiepnguyenquoc@gmail.com>
2015-12-29 22:31:07 +0700

    changed domain to 6sao.vn

--- Survey/app/src/main/java/com/nghiepnguyen/survey/networking/Endpoint.java
+++ Survey/app/src/main/java/com/nghiepnguyen/survey/networking/Endpoint.java
@@ -7,6 +7,6 @@ public class Endpoint {
     public static final String SURVEY_SIGN_IN = "sign_in";
 
     public static final String COUNTRY_CODE = "http://www.w3schools.com/website/customers_mysql.php";
-    public static final String LOGIN="http://www.ebas.vn/api/admin/login";
-    public static final String PROJECT_LIST="http://www.ebas.vn/api/admin/getProjecList";
+    public static final String LOGIN="http://www.6sao.vn/api/admin/login";
+    public static final String PROJECT_LIST="http://www.6sao.vn/api/admin/getProjecList";
 }
