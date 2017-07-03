9ddc12d056d6a9940a812359c04d534c960a26ba
Nghiep Nguyen <nghiep.nguyen@inspireventures.com>
2017-07-03 21:36:14 +0700

    record audio

--- Survey/app/src/main/java/com/nghiepnguyen/survey/application/MainApplication.java
+++ Survey/app/src/main/java/com/nghiepnguyen/survey/application/MainApplication.java
@@ -2,13 +2,16 @@ package com.nghiepnguyen.survey.application;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.github.lassana.recorder.AudioRecorder;
 
 /**
  * Created by nghiep on 10/29/15.
  */
 public class MainApplication extends Application {
-
     private static MainApplication instance;
+    private AudioRecorder mAudioRecorder;
 
     public MainApplication() {
         super();
@@ -25,10 +28,16 @@ public class MainApplication extends Application {
         Foreground.init(this);
     }
 
+    public void setRecorder(@NonNull AudioRecorder recorder) {
+        mAudioRecorder = recorder;
+    }
+
+    public AudioRecorder getRecorder() {
+        return mAudioRecorder;
+    }
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
     }
-
-
 }
