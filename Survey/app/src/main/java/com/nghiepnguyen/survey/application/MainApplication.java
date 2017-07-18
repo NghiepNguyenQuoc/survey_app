2ef770207290c4cb3aeae9efbb46fc31224ddb4a
Nghiep Nguyen <nghiep.nguyen@inspireventures.com>
2017-07-18 09:33:15 +0700

    removed useless file

--- Survey/app/src/main/java/com/nghiepnguyen/survey/application/MainApplication.java
+++ Survey/app/src/main/java/com/nghiepnguyen/survey/application/MainApplication.java
@@ -2,16 +2,13 @@ package com.nghiepnguyen.survey.application;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.annotation.NonNull;
-
-import com.github.lassana.recorder.AudioRecorder;
 
 /**
  * Created by nghiep on 10/29/15.
  */
 public class MainApplication extends Application {
     private static MainApplication instance;
-    private AudioRecorder mAudioRecorder;
+    //private AudioRecorder mAudioRecorder;
 
     public MainApplication() {
         super();
@@ -28,13 +25,13 @@ public class MainApplication extends Application {
         Foreground.init(this);
     }
 
-    public void setRecorder(@NonNull AudioRecorder recorder) {
+    /*public void setRecorder(@NonNull AudioRecorder recorder) {
         mAudioRecorder = recorder;
     }
 
     public AudioRecorder getRecorder() {
         return mAudioRecorder;
-    }
+    }*/
 
     @Override
     protected void attachBaseContext(Context base) {
