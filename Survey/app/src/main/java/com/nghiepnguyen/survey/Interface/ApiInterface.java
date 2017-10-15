0f8d93e5947d333f93a709f833a4b8025e93065e
Nghiep Nguyen <nghiep.nguyen@inspireventures.com>
2017-10-15 16:27:06 +0700

    add all method call API

--- Survey/app/src/main/java/com/nghiepnguyen/survey/Interface/ApiInterface.java
+++ Survey/app/src/main/java/com/nghiepnguyen/survey/Interface/ApiInterface.java
@@ -5,13 +5,35 @@ import com.nghiepnguyen.survey.model.ProjectModel;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Created by W10-PRO on 21-Sep-17.
  */
 
 public interface ApiInterface {
-    @GET("/getProjecList")
-    Call<List<ProjectModel>> getProjectList();
+    @POST("/api/admin/MemberLogin")
+    @FormUrlEncoded
+    Call<List<ProjectModel>> postLogin(@Field("userName") String userName, @Field("password") String password);
+
+    @POST("/api/admin2/SaveSurvey")
+    @FormUrlEncoded
+    Call<List<ProjectModel>> postResultSurvey(@Field("FullName") String FullName, @Field("NumberID") String NumberID
+            , @Field("PhoneNumber") String PhoneNumber, @Field("Address") String Address, @Field("Email") String Email
+            , @Field("ProjectID") String ProjectID, @Field("IsCompeleted") String IsCompeleted, @Field("GPSLatitude") String GPSLatitude
+            , @Field("GPSLongitude") String GPSLongitude, @Field("GPSAddress") String GPSAddress, @Field("GPSTime") String GPSTime
+            , @Field("Action") String Action, @Field("Data") String Data);
+
+    @GET("/api/admin/getProjecList")
+    Call<List<ProjectModel>> getProjectList(@Query("UserId") int UserId, @Query("SecrectToken") String SecrectToken);
+
+    @GET("/api/admin/getquestionarelist")
+    Call<List<ProjectModel>> getProjectData(@Query("projectID") int projectID);
+
+    @GET("/api/admin/GetQuestionareConditionList")
+    Call<List<ProjectModel>> getProjectRoute(@Query("ProjectIDCondition") int ProjectIDCondition);
 }
